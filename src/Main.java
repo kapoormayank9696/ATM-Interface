11148f88a59a5ffdce5150a471096bfefdb423b4
kapoormayank9696 <kapoormayank645@gmail.com>
2026-07-29 19:37:27 +0530

    ATM Interfaces Project All Files Create Successfully

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         BankAccount account = new BankAccount(
                 1234567890,
                 "Mayank",
